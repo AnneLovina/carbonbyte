dbea91096145fc3674ba4938e22e011fc6e9ecd4
Niclas Wuestenbecker <niclaswue@gmail.com>
2025-01-31 07:10:53 +0100

    add landing and imprint and privacy

--- routes/main.py
+++ routes/main.py
@@ -2,6 +2,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from flask_login import login_required, current_user
 from config import CALCULATOR_CONFIG
+from config import LANDING_PAGE_CONFIG
 from calculations import calculate
 
 main = Blueprint("main", __name__)
@@ -9,7 +10,7 @@ main = Blueprint("main", __name__)
 
 @main.route("/")
 def index():
-    return redirect(url_for("auth.login"))
+    return render_template("landing.html", config=LANDING_PAGE_CONFIG)
 
 
 @main.route("/profile")
@@ -28,3 +29,13 @@ def calc():
         raw_string = json.dumps(results, indent=4)
         return render_template("results.html", raw=raw_string, **results)
     return render_template("calculator.html", config=CALCULATOR_CONFIG)
+
+
+@main.route("/imprint")
+def imprint():
+    return render_template("imprint.html", config=LANDING_PAGE_CONFIG)
+
+
+@main.route("/privacy")
+def privacy():
+    return render_template("privacy.html", config=LANDING_PAGE_CONFIG)
